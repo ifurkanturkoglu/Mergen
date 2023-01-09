d8d0b5e040413a5b713c6b513c607137dfa02fed
EgeDogantemur <egedogantemur26@gmail.com>
2023-01-09 16:06:28 +0300

    Update b.cs
    commit

--- Assets/b.cs
+++ Assets/b.cs
@@ -7,7 +7,7 @@ public class b : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("selamama");
+        Debug.Log("selamamafff");
     }
 
     // Update is called once per frame
