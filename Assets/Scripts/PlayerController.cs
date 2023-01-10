7104b6d5a1c80c5f4017df6848e539d0a10eaf4c
Mustafa Sarıbaş <mustafasrbs1@gmail.com>
2023-01-10 20:58:54 +0300

    aa
    aa

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,7 +26,7 @@ public class PlayerController : MonoBehaviour
         if (direction.magnitude>=0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg+cam.eulerAngles.y;
-            float angle = targetAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            float angle =  Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f)*Vector3.forward;
             controller.Move(moveDirection.normalized * speed * Time.deltaTime);
