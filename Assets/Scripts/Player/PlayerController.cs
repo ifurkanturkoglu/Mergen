59c1566daceea4478d98d6e6aa4e030679c9b952
Betül Kara <51272636+betul-kara@users.noreply.github.com>
2023-01-12 20:42:04 +0300

    Script Fixed
    PController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,38 +1,32 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class PlayerController : MonoBehaviour
-//{
-//    [SerializeField] float speed = 6f;
-//    [SerializeField] float turnSmoothTime = 0.1f;
-//    [SerializeField] float turnSmoothVelocity;
-//    [SerializeField] Transform cam;
-//    CharacterController controller;
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 6f;
+    [SerializeField] float turnSmoothTime = 0.1f;
+    [SerializeField] float turnSmoothVelocity;
+    [SerializeField] Transform cam;
+    CharacterController controller;
 
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
 
-
-//    void Start()
-//    {
-//        controller = GetComponent<CharacterController>();
-//    }
-
-
-//    void Update()
-//    {
-//        float horizontal = Input.GetAxisRaw("Horizontal");
-//        float vertical = Input.GetAxisRaw("Vertical");
-//        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-//        if (direction.magnitude>=0.1f)
-//        {
-//            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg+cam.eulerAngles.y;
-//            float angle =  Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-//            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-//            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f)*Vector3.forward;
-//            //controller.Move(moveDirection.normalized * speed * Time.deltaTime);
-
-//        }
-
-//    }
-   
-//}
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+}
