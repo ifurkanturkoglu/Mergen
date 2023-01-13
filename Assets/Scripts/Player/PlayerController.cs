bde547e70beae3588ed994fb834454a4c83139f4
Mustafa Sarıbaş <mustafasrbs1@gmail.com>
2023-01-13 14:09:22 +0300

    aa
    aa

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed = 6f;
-    [SerializeField] float turnSmoothTime = 0.1f;
-    [SerializeField] float turnSmoothVelocity;
-    [SerializeField] Transform cam;
+    //[SerializeField] float speed = 6f;
+    //[SerializeField] float turnSmoothTime = 0.1f;
+    //[SerializeField] float turnSmoothVelocity;
+    //[SerializeField] Transform cam;
     CharacterController controller;
 
 
@@ -20,18 +20,18 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-        if (direction.magnitude>=0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg+cam.eulerAngles.y;
-            float angle =  Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f)*Vector3.forward;
-            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        //float horizontal = Input.GetAxisRaw("Horizontal");
+        //float vertical = Input.GetAxisRaw("Vertical");
+        //Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        //if (direction.magnitude>=0.1f)
+        //{
+        //    float targetAngle = Mathf.Atan2(direction.x, direction.z) *Mathf.Rad2Deg+cam.eulerAngles.y;
+        //    float angle =  Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        //    transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        //    Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f)*Vector3.forward;
+        //    controller.Move(moveDirection.normalized * speed * Time.deltaTime);
 
-        }
+        //}
 
     }
    
