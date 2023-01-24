b147656f5e0f451a1eade5b4e3c318025d2e8b52
Betül Kara <51272636+betul-kara@users.noreply.github.com>
2023-01-24 18:02:08 +0300

    New Asset
    Added WeaponController.cs

---
+++ Assets/Scripts/Player/WeaponController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    bool isStrafe = false;
+
+    private void Update()
+    {
+        HandleInput();
+        HandleStrafe();
+    }
+
+    private void HandleStrafe()
+    {
+        PController.Instance.Anim.SetBool("IsStrafe", isStrafe);
+
+        if (isStrafe == true)
+        {
+            GetComponent<PController>().movementType = PController.MovementType.Strafe;
+        }
+        if (isStrafe == false)
+        {
+            GetComponent<PController>().movementType = PController.MovementType.Directional;
+        }
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isStrafe = !isStrafe;
+        }
+    }
+}
